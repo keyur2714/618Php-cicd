19fdd9eb8d660e8f06c55451c85c4a523e1ce54c
keyur2714 <keyurjava27@gmail.com>
2023-04-16 11:33:04 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<h1>Hello World....!</h1>
+<html>
+	<head>
+		<style>
+			body {background-color: powderblue;}
+			h1{
+				color : red;
+			}
+			li{
+				color : blue;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Friends...!</h1>
+		<ul>
+			<li>Keyur</li>
+			<li>Denish</li>
+			<li>Vinit</li>
+		</ul>
+	</body>
+</html>
